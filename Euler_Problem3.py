ba787a19e4d9588a95fcc1f072d1d372ec05eb3b
Faruk GÜNGÖR <farcompen71@gmail.com>
2017-11-28 16:09:05 +0300

    Update Euler_Problem3.py

--- Euler_Problem3.py
+++ Euler_Problem3.py
@@ -1,5 +1,11 @@
 #! /usr/bin python
 # -*- coding: utf-8 -*-
+"""
+he prime factors of 13195 are 5, 7, 13 and 29.
+
+What is the largest prime factor of the number 600851475143 ?
+
+"""
 
 from datetime import datetime
 
